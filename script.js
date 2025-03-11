8878ee9e4dab2815752c9bd8704df2d676f25175
Kanekiteam <125080687+Kanekiteam@users.noreply.github.com>
2025-03-10 22:24:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
-const glitchText = document.querySelector('.glitch');
+function detectarDispositivo() {
+    let infoDispositivo = document.querySelector('.device-info');
+    let ancho = window.innerWidth;
+    
+    if (ancho <= 600) {
+        infoDispositivo.innerText = "📱 Estás en un dispositivo móvil.";
+    } else if (ancho <= 900) {
+        infoDispositivo.innerText = "📟 Estás en una tablet.";
+    } else {
+        infoDispositivo.innerText = "🖥️ Estás en una PC.";
+    }
+}
 
-glitchText.addEventListener('mouseenter', () => {
-    glitchText.style.animation = 'glitch 0.5s infinite alternate';
-});
 
-glitchText.addEventListener('mouseleave', () => {
-    glitchText.style.animation = 'glitch 1.5s infinite alternate';
-});
+window.onload = detectarDispositivo;
+window.onresize = detectarDispositivo;
